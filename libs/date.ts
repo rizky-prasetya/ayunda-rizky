694f00d6a5a7fd03b52e61cd02f4d7c0035a258d
Rizky Prasetya <rizkyprasetya@duck.com>
2024-03-15 12:38:59 +0700

    fix date formatter

--- libs/date.ts
+++ libs/date.ts
@@ -7,13 +7,16 @@ export function formatDate(date: Date, format: string) {
         s: date.getSeconds(),
     };
 
-    format = format.replace(/(M+|d+|h+|m+|s+)/g, (v) =>
-        ((v.length > 1 ? '0' : '') + z[v as keyof typeof z]).slice(-2),
-    );
+    return format
+        .replace(/(M+|d+|h+|m+|s+)/g, (v) =>
+            (
+                (v.length > 1 ? '0' : '') + z[v.slice(-1) as keyof typeof z]
+            ).slice(-2),
+        )
 
-    return format.replace(/(y+)/g, (v) =>
-        date.getFullYear().toString().slice(-v.length),
-    );
+        .replace(/(y+)/g, (v) =>
+            date.getFullYear().toString().slice(-v.length),
+        );
 }
 
 export function compareDesc(dateLeft: Date, dateRight: Date): number {
