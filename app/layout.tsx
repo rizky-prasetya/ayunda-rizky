9b0abc4d93eac6dea8ffa4c358e494aaf6cfe80b
Rizky Prasetya <rizkyprasetya@duck.com>
2024-03-15 22:11:54 +0700

    change default html lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="ko" className={fontVariable}>
+        <html lang="id-ID" className={fontVariable}>
             <PreloadResources />
             <body className="font-serif">
                 {children}
