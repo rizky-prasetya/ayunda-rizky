9a4cb41e44ca0f2e1e442b7a530a803ad687a567
Rizky Prasetya <rizkyprasetya@duck.com>
2024-03-21 20:37:31 +0700

    change select option text style

--- components/select.tsx
+++ components/select.tsx
@@ -19,7 +19,7 @@ export default function SelectGroup({
             value={selectedOption}
             onChange={(e) => setSelectedOption(e.target.value)}
             className={cn(
-                'flex h-10 w-full rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                'flex h-10 w-full rounded-md border px-3 py-2 text-sm ring-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
                 className,
             )}
             {...props}
