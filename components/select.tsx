b3e28bbc8bb188a94327740ede7a3cba25fee127
Rizky Prasetya <rizkyprasetya@duck.com>
2024-03-21 20:23:44 +0700

    change select option text style

--- components/select.tsx
+++ components/select.tsx
@@ -11,34 +11,29 @@ export default function SelectGroup({
     HTMLSelectElement
 > & {
     selectList: { text: string; value: string; disabled?: boolean }[];
-    className?: string;
 }) {
     const [selectedOption, setSelectedOption] = useState<string>('');
 
     return (
-        <div className="relative z-20 bg-transparent">
-            <select
-                value={selectedOption}
-                onChange={(e) => setSelectedOption(e.target.value)}
-                className={cn(
-                    'flex h-10 w-full rounded-md border px-3 py-2 text-sm ring-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-                    className,
-                )}
-                {...props}
-            >
-                {selectList.map(({ text, value, disabled }, i) => (
-                    <option
-                        key={i}
-                        value={value}
-                        className={
-                            disabled ? 'text-muted-foreground' : 'text-body'
-                        }
-                        disabled={disabled}
-                    >
-                        {text}
-                    </option>
-                ))}
-            </select>
-        </div>
+        <select
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+            className={cn(
+                'flex h-10 w-full rounded-md border px-3 py-2 text-sm ring-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                className,
+            )}
+            {...props}
+        >
+            {selectList.map(({ text, value, disabled }, i) => (
+                <option
+                    key={i}
+                    value={value}
+                    className="text-body"
+                    disabled={disabled}
+                >
+                    {text}
+                </option>
+            ))}
+        </select>
     );
 }
