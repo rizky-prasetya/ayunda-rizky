319442f097bef80c23eab800055c9f4951f457d5
Rizky Prasetya <52526699+rizky-prasetya@users.noreply.github.com>
2024-04-16 15:45:39 +0000

    feat: format time

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -24,25 +24,27 @@ export default function Countdown() {
     const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
 
+    const format = (value: number) => `0${value}`.slice(-2);
+
     return (
         <div className="flex items-center justify-center space-x-4 mt-4">
             <div className="flex flex-col items-center px-4">
-                <span className="text-4xl lg:text-5xl text-gray-200">{days}</span>
+                <span className="text-4xl leading-7 lg:text-5xl">{format(days)}</span>
                 <span className="text-gray-400 mt-2">Hari</span>
             </div>
             <span className="w-[1px] h-24 bg-gray-400"></span>
             <div className="flex flex-col items-center px-4">
-                <span className="text-4xl lg:text-5xl text-gray-200">{hours}</span>
+                <span className="text-4xl leading-7 lg:text-5xl">{format(hours)}</span>
                 <span className="text-gray-400 mt-2">Jam</span>
             </div>
             <span className="w-[1px] h-24 bg-gray-400"></span>
             <div className="flex flex-col items-center px-4">
-                <span className="text-4xl lg:text-5xl text-gray-200">{minutes}</span>
+                <span className="text-4xl leading-7 lg:text-5xl">{format(minutes)}</span>
                 <span className="text-gray-400 mt-2">Menit</span>
             </div>
             <span className="w-[1px] h-24 bg-gray-400"></span>
             <div className="flex flex-col items-center px-4">
-                <span className="text-4xl lg:text-5xl text-gray-200">{seconds}</span>
+                <span className="text-4xl leading-7 lg:text-5xl">{format(seconds)}</span>
                 <span className="text-gray-400 mt-2">Detik</span>
             </div>
         </div>
