554e9423b2b586a74313a8b001f6d78ce73f2966
Rizky Prasetya <52526699+rizky-prasetya@users.noreply.github.com>
2024-04-22 09:38:42 +0000

    feat: attendance option checkbox

--- components/comment-form-dialog.tsx
+++ components/comment-form-dialog.tsx
@@ -86,7 +86,31 @@ export default function CommentFormDialog() {
                                 className="flex-[2]"
                             />
                         </div>
-                        <SelectGroup
+
+                        <div className="grid grid-cols-1 gap-3 mt-3 sm:grid-cols-2">
+                            <label>
+                                <input type="radio" value={1} className="peer hidden" name="attendance"/>
+                                
+                                <div className="hover:bg-gray-50 flex items-center justify-between px-4 py-2 border-2 rounded-lg cursor-pointer text-sm border-gray-200 group peer-checked:border-blue-500">
+                                    <h2 className="font-medium text-gray-700">Hadir</h2>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-9 h-9 text-blue-600 invisible group-[.peer:checked+&]:visible">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </div>
+                            </label>
+
+                            <label>
+                                <input type="radio" value={0} className="peer hidden" name="attendance" />
+                                
+                                <div className="hover:bg-gray-50 flex items-center justify-between px-4 py-2 border-2 rounded-lg cursor-pointer text-sm border-gray-200 group peer-checked:border-blue-500">
+                                    <h2 className="font-medium text-gray-700">Tidak hadir</h2>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-9 h-9 text-blue-600 invisible group-[.peer:checked+&]:visible">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+                                </div>
+                            </label>
+                        </div>
+                        {/* <SelectGroup
                             name="attendance"
                             className="mt-3"
                             selectList={[
@@ -104,7 +128,7 @@ export default function CommentFormDialog() {
                                     value: 'false',
                                 },
                             ]}
-                        />
+                        /> */}
                         <Textarea
                             disabled={loading}
                             name="message"
